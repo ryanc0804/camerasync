3a020d2bff592a1d2372e8591487e637ef7a4fde
ParkerM6767 <parker@lamay.systems>
2026-07-07 15:38:56 -0400

    commit for database schema script

--- apps/server/src/db/schema.sql
+++ apps/server/src/db/schema.sql
@@ -1,1 +1,48 @@
-CREATE DATABASE IF NOT EXISTS knighthyve
+CREATE TABLE IF NOT EXISTS users (
+    user_id     SERIAL PRIMARY KEY,
+    email       VARCHAR(255) NOT NULL UNIQUE,
+    display_name VARCHAR(63) DEFAULT NULL,
+    profile_picture TEXT,
+    roles       JSON NOT NULL DEFAULT '[]',
+    password    TEXT NOT NULL,
+    organizations JSON DEFAULT NULL,
+    settings JSON NOT NULL DEFAULT '{}',
+    agreements JSON NOT NULL DEFAULT '[]'
+);
+
+CREATE TABLE IF NOT EXISTS sessions (
+    user_id     BIGINT NOT NULL,
+    session_token TEXT,
+    csrf_token TEXT,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+    device VARCHAR(32),
+    expires TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS organizations (
+    org_id VARCHAR(255) PRIMARY KEY,
+    name VARCHAR(64),
+    admins JSON DEFAULT '[]',
+    rec_sessions JSON DEFAULT '[]'
+)
+
+CREATE TABLE IF NOT EXISTS videos (
+    recording_uri TEXT,
+    session_id VARCHAR(255),
+    author BIGINT
+)
+
+CREATE TABLE IF NOT EXISTS recordings (
+    sessions    JSON DEFAULT '[]',
+    org_id      VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP NOT NULL DEFAULT NOW()
+)
+
+CREATE TABLE IF NOT EXISTS posts (
+    post_id     VARCHAR(255) PRIMARY KEY,
+    author      BIGINT NOT NULL,
+    content     TEXT,
+    created_at  TIMESTAMP DEFAULT NOW(),
+    video_meta  JSON,
+    reply       VARCHAR(255)
+)
